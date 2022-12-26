71b01f680016be75ee204b65e50b2497c71bd681
SaumyaKinnariKartikDalal <98890702+SaumyaKinnariKartikDalal@users.noreply.github.com>
2022-12-26 21:31:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,24 @@ function snap(){
 }
 
 console.log("ml5.version",ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hcBV1BSns/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/DESYG5SCH/model.json', modelLoaded);
 
 function modelLoaded(){
-    console.log("MOdel Loaded")
+    console.log("Model Loaded")
+}
+
+function check(){
+    img = document.getElementById("selfie");
+    classifier.classify(img, result);
+}
+
+function result (error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results[0]);
+        document.getElementById("resultobj").innerHTML = results[0].label; 
+        document.getElementById("objaccuracy").innerHTML = (results[0].confidence*100).toFixed(2) + "%"; 
+    }
 }
